470b4dd41d6eaf0f09da97813089de5703d8d9be
tprat <Titouan.p@hotmail.fr>
2021-12-03 00:58:44 +0100

    logo cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,10 @@
 Item.destroy_all
 User.destroy_all
 
-5.times do
-    Item.create(title: Faker::Creature::Cat.name, description: Faker::Lorem.sentence(word_count: 3, supplemental: true), price: Faker::Number.decimal(l_digits: 2), image_url: "/images/cat-image-#{rand(6)}.jpg")
+img = ["https://images.pexels.com/photos/171227/pexels-photo-171227.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/731553/pexels-photo-731553.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/162287/cat-british-shorthair-bkh-thoroughbred-162287.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/1492219/pexels-photo-1492219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/596590/pexels-photo-596590.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/1404821/pexels-photo-1404821.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/2527491/pexels-photo-2527491.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"]
+
+7.times do |x|
+    Item.create(title: Faker::Creature::Cat.name, description: Faker::Lorem.sentence(word_count: 10, supplemental: true), price: Faker::Number.decimal(l_digits: 2), image_url: img[x])
 end
 
 user = User.create(email: "user@test.com", password: "123456789")

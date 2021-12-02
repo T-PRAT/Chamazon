e557d0263a4aff07d3d97e29a2d74557524c5029
tprat <Titouan.p@hotmail.fr>
2021-12-02 01:46:36 +0100

    stripe

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,57 +3,17 @@ class ItemsController < ApplicationController
   before_action :save_items_in_cart
   before_action :set_item, only: %i[ show edit update destroy ]
 
-  # GET /items or /items.json
   def index
     @items = Item.all
     @join_table_item_cart = JoinTableItemsCart.new
   end
 
-  # GET /items/1 or /items/1.json
   def show
     @items = Item.find(params[:id])
     @join_table_item_cart = JoinTableItemsCart.new
-  end
-
-  # GET /items/new
-  def new
-    @item = Item.new
-  end
-
-  # GET /items/1/edit
-  def edit
-  end
-
-  # POST /items or /items.json
-  def create
-    @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /items/1 or /items/1.json
-  def update
-  end
-
-  # DELETE /items/1 or /items/1.json
-  def destroy
-    @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
     # Use callbacks to share common setup or constraints between actions.
+  end
+  private
     def set_item
       @item = Item.find(params[:id])
     end
